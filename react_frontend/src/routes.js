7ec12cb3a1174d0d145c4938867a25f8f5347b39
Gustavo Inácio <gustavoinacio@ua.pt>
2020-04-04 18:10:54 +0100

    Alteração no routes.js

--- react_frontend/src/routes.js
+++ react_frontend/src/routes.js
@@ -22,6 +22,7 @@ import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
+import Accidents from "views/examples/Accidents.js"
 
 var routes = [
   {
